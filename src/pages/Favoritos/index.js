c822efd449a4d0545769ce0aa8d5717300429700
luizpcosta77@gmail.com <luizPcosta77@gmai.com>
2025-01-16 10:07:31 -0300

    botao excluir criado

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -11,20 +11,38 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilme = filmes.filter( (item) => {
+            return (item.id !== id)   // retorna todos os item menos o que foi clicado 'excluido'
+        })
+
+        setFilmes(filtroFilme);
+        localStorage.setItem('@saraFlix', JSON.stringify(filtroFilme) )
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Minha Lista</h1>
+
+            {filmes.length === 0 && <span> você não possui nenhum filme salvo</span> }
 
             <ul>
                 {filmes.map((item)=>{
                     return(
                         <li key={item.id}>
-                            <span>{item.title}</span>
+                            <div className='container'>
+                                <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+                                <span>{item.title}</span>
+                            </div>
+                            <div>
+                                <h3>{item.tagline}</h3>
+                            </div>
                             <div>
                                 <Link to={`/filmes/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
+                        
                     )
                 })}
             </ul>
